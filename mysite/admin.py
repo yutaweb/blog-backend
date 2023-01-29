b93f39443c2420837804533a88fd1bf7d6970647
yutaweb <yuta3984@gmail.com>
2023-01-30 02:31:55 +0900

    ZLWS-3 管理画面の利用

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from mysite.models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email',
+                'password',
+            )
+        }),
+        (None, {
+            'fields': (
+                'is_active',
+                'is_admin',
+            )
+        })
+    )
+
+    list_display = ('email', 'is_active')
+    list_filter = ()
+    ordering = ()
+    filter_horizontal = ()
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'password',),
+        }),
+    )
+
+admin.site.unregister(Group)
+admin.site.register(User, CustomUserAdmin)
