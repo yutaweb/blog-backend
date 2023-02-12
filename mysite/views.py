0eba92ac105bb42897786c47071faa295342f49a
yutaweb <yuta3984@gmail.com>
2023-02-12 21:23:39 +0900

    BLOG-4 記事一覧＆詳細の実装

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 
 def index(request):
+    objs = Article.objects.all()[:3]
     context = {
-        'title': 'toyama'
+        'title': 'toyama',
+        'articles': objs,
     } 
     return render(request, 'mysite/index.html', context)
