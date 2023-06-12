f6cd82c37c984e7d24e2ae1fe0c689eec154e7a0
yutaweb <yuta3984@gmail.com>
2023-06-13 05:45:27 +0900

    軽微な修正

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='list'),
-    path('tags/<slug:slug>/', views.tags),
-    # path('<slug:pk>/', views.article, name='detail'),
+    path('tags/<slug:slug>/', views.tags, name='tags'),
     path('<slug:pk>/', views.ArticleView.as_view(), name='detail'),
     path('<slug:pk>/like/', views.like),
 ]
